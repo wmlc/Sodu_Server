d249a2be7f4108f8b45a69fb4a5d38de3dd6738f
dang <ruobin521@gmail.com>
2018-03-23 09:21:15 +0800

    a

--- router/rank.js
+++ router/rank.js
@@ -1,16 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-function getNum(fuc) {
-    fuc
-}
-
-
 router.get('/', function(req, res) {
-    getNum(() => {
-        res.send('pageIndex = 0');
-    })
-
+    res.send('pageIndex = 0');
 });
 
 router.get('/:pageIndex', function(req, res) {
