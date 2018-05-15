5f6fd89c529a8a0275f7561b4a73f152b940830f
dang <ruobin521@gmail.com>
2018-05-15 11:09:04 +0800

    解析catalogpageurl

--- api/htmlAnalyse.js
+++ api/htmlAnalyse.js
@@ -11,97 +11,16 @@ function getContent(html, url, bookid, type) {
     if (!sourceItem) {
         return null
     }
-    if (type == contentType.contet) {
-        result = getHtmlCommon(html, sourceItem.contentReg)
-    } else if (type == contentType.catalogUrl) {
-
+    if (type == contentType.content) {
+        var content = getHtmlCommon(html, sourceItem.contentReg)
+        var catalogUrl = getCatalogPageUrl(html, url, bookid)
+        result = {
+            content,
+            catalogUrl
+        }
     } else if (type == contentType.catalgs) {
 
     }
-    // switch (uri.host) {
-    //     //少年文学
-    //     case source.snwx.host:
-    //         if (type == contentType.contet) {
-    //             result = getHtmlCommon(html, source.snwx.contentReg)
-    //         } else if (type == contentType.catalogUrl) {
-
-    //         } else if (type == contentType.catalgs)
-    //             break;
-    //         //大海中文
-    //     case source.dhzw.host:
-    //         result = getHtmlCommon(html, /<div id=\"BookText\">.*?<\/div>/)
-    //         break;
-    //         //爱上中文
-    //     case source.aszw.host:
-    //         result = getHtmlCommon(html, /<div id=\"contents\".*?<\/div>/)
-    //         break;
-    //         //七度
-    //     case source.qdsw.host:
-    //         result = getHtmlCommon(html, /<div id=\"BookText\">.*?<\/div>/)
-    //         break;
-    //         //云来阁
-    //     case source.ylg.host:
-    //         result = getHtmlCommon(html, /<div id=\"content\">.*?<div class=\"bottomlink tc\">/)
-    //         break;
-    //         //古古小说网
-    //     case source.ggxs.host:
-    //         result = getHtmlCommon(html, /<dd id=\"contents\".*?<\/dd>/)
-    //         break;
-    //         //风云小说网
-    //     case source.fyxs.host:
-    //         result = getHtmlCommon(html, /<p id=\"?content\"?.*?<\/p>/)
-    //         break;
-    //         //第九中文网
-    //     case source.djzw.host:
-    //         result = getHtmlCommon(html, /<div id=\"?content\"?.*?<\/div>/)
-    //         break;
-    //         //手牵手小说
-    //     case source.sqs.host:
-    //         result = getHtmlCommon(html, /<div id=\"content\">.*?<\/div>/)
-    //         break;
-    //         //风华居
-    //     case source.fhj.host:
-    //         result = getHtmlCommon(html, /<div id=\"content\">.*?<\/div>/)
-    //         break;
-    //         //木鱼哥
-    //     case source.myg.host:
-    //         result = getHtmlCommon(html, /<p class=\"vote\">.*?<\/div>/)
-    //         break;
-    //         //乐文小说网
-    //     case source.lwxs.host:
-    //         result = getHtmlCommon(html, /<div id=\"txtright\">.*?<span id=\"endtips\">/)
-    //         break;
-    //         //卓雅居小说网
-    //     case source.zyjxs.host:
-    //         result = getHtmlCommon(html, /<div id=\"?content\"?.*?<\/div>/)
-    //         break;
-    //         //八一小说网
-    //     case source.byxsw.host:
-    //         result = getHtmlCommon(html, /<div id=\"?content\"?[\s\S]*?<\/div>/)
-    //         break;
-    //         //大书包小说网
-    //     case source.dsb.host:
-    //         result = getHtmlCommon(html, /<div class=\"hr101\">.*?<span id=\"endtips\">/)
-    //         break;
-    //         //漂流地文学
-    //     case source.pldwx.host:
-    //         result = getHtmlCommon(html, /<div id=\"BookText\">.*?<\/div>/)
-    //         break;
-    //         //齐鲁文学
-    //     case source.qlwx.host:
-    //         result = getHtmlCommon(html, /<div id=\"content\">.*?<\/div>/)
-    //         break;
-    //         //一流吧
-    //     case source.ylb.host:
-    //         result = getHtmlCommon(html, /<\/tr><\/table>.*?<br\/>.*?<center>/)
-    //         break;
-    //         //乐安宣书网
-    //     case source.lax.host:
-    //         result = getHtmlCommon(html, /<div id="content"[\s\S]*?<\/div>/)
-    //         break;
-    //     default:
-    //         break;
-    // }
     return result;
 }
 
@@ -122,6 +41,11 @@ function getHtmlCommon(html, regexStr) {
     }
 }
 
+
+function getCatalogPageUrl(html, url, bookid) {
+    let result = url.substr(0, url.lastIndexOf('/') + 1)
+    return result;
+}
 
 function replaceSymbel(html) {
     html = html.replace(/<br.*?\/>/g, "\n");
--- model/contentType.js
+++ model/contentType.js
@@ -1,5 +1,4 @@
 module.exports = {
     content: 1,
     catalgs: 2,
-    catalogUrl: 3
 }
