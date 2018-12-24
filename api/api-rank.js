27b5089dedd7c8800a41e0dc27fef7e075e2de08
dang <ruobin521@gmail.com>
2018-12-24 17:15:00 +0800

    fix bug

--- api/api-rank.js
+++ api/api-rank.js
@@ -1,46 +1,47 @@
-import axios from 'axios';
-import url from '../core/soduUrl';
+import axios from 'axios'
+import url from '../core/soduUrl'
 import Book from '../model/book'
-const resultCode = require('../api/api-resultCode');
+const resultCode = require('../api/api-resultCode')
 var moment = require('moment')
-var iconv = require('iconv-lite');
+// var iconv = require('iconv-lite')
 
-function getRankBooks(html) {
-    let str = html
-    let books = []
-    let bookReg = /<div class="main-html".[\s\S]*?<div style="width:88px;float:left;">.[\s\S]*?<\/div>/g
-    let listReg = /<a href="(.[\S\s]*?)".*?>.*?addToFav\((.[\S\s]*?), '(.[\S\s]*?)'.[\S\s]*?<a.[\S\s]*?>(.[\S\s]*?)<\/a>.[\S\s]*?width:88.*>(.[\S\s]*?)<\/div>/
+function getRankBooks (html) {
+  let str = html
+  let books = []
+  let bookReg = /<div class="main-html".[\s\S]*?<div style="width:88px;float:left;">.[\s\S]*?<\/div>/g
+  let listReg = /<a href="(.[\S\s]*?)".*?>.*?addToFav\((.[\S\s]*?), '(.[\S\s]*?)'.[\S\s]*?<a.[\S\s]*?>(.[\S\s]*?)<\/a>.[\S\s]*?width:88.*>(.[\S\s]*?)<\/div>/
 
-    let matches = str.match(bookReg)
-    let book
-    matches.forEach(element => {
-        var match = element.match(listReg)
-        book = new Book()
-        book.bookId = match[2]
-        book.bookName = match[3]
-        book.newestCatalogName = match[4].replace(/【.*?】/, '')
-        book.updatePageUrl = match[1]
-        book.updateTime = moment(match[5], 'YYYY/MM/DD hh:mm:ss').format('YYYY/MM/DD hh:mm')
-        books.push(book)
-    })
-    return books
+  let matches = str.match(bookReg)
+  let book
+  matches.forEach(element => {
+    var match = element.match(listReg)
+    book = new Book()
+    book.bookId = match[2]
+    book.bookName = match[3]
+    book.newestCatalogName = match[4].replace(/【.*?】/, '')
+    book.updatePageUrl = match[1]
+    book.updateTime = moment(match[5], 'YYYY/MM/DD hh:mm:ss').format(
+      'YYYY/MM/DD hh:mm'
+    )
+    books.push(book)
+  })
+  return books
 }
 
-
-async function getRank(index) {
-    let uri = url.getRankUrl(index)
-    try {
-        var result = await axios({
-            method: 'get',
-            url: uri
-        })
-        var books = getRankBooks(result.data)
-        let result = resultCode.createResult(resultCode.success, books)
-        return result
-    } catch (e) {
-        let result = resultCode.createResult(resultCode.faild, e.message)
-        return result
-    }
+async function getRank (index) {
+  let uri = url.getRankUrl(index)
+  try {
+    var res = await axios({
+      method: 'get',
+      url: uri
+    })
+    var books = getRankBooks(res.data)
+    let result = resultCode.createResult(resultCode.success, books)
+    return result
+  } catch (e) {
+    let result = resultCode.createResult(resultCode.faild, e.message)
+    return result
+  }
 }
 
 module.exports = { getRank }
--- router/router-rank.js
+++ router/router-rank.js
@@ -1,14 +1,13 @@
-var express = require('express');
+var express = require('express')
 var rank = require('../api/api-rank')
-import axios from 'axios';
 
-var router = express.Router();
-router.get('/', function(req, res) {
-    let index = req.query.index
-    index = index ? parseInt(index) : 1
-    rank.getRank(index).then(result => {
-        res.send(result)
-    })
+var router = express.Router()
+router.get('/', function (req, res) {
+  let index = req.query.index
+  index = index ? parseInt(index) : 1
+  rank.getRank(index).then(result => {
+    res.send(result)
+  })
 })
 
-module.exports = router;
+module.exports = router
